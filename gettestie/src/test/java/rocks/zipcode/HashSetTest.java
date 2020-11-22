e397e13c46d9267ae026cde4023eca3810b894a3
Peter <pgkutc@gmail.com>
2020-11-21 20:17:01 -0500

    testing push

---
+++ gettestie/src/test/java/rocks/zipcode/HashSetTest.java
@@ -0,0 +1,25 @@
+package rocks.zipcode;
+
+import org.junit.Test;
+
+import java.util.HashSet;
+
+public class HashSetTest {
+
+    @Test
+    public void hashSetTest() {
+        HashSet<Person> kutchenHouse = new HashSet<>();
+
+        Person familyMember1 = new Person("Ken Kutchen", 1957);
+        Person familyMember2 = new Person("Kathy Kutchen", 1958);
+        Person familyMember3 = new Person("Juliann Kutchen", 1985);
+        Person familyMember4 = new Person("Amy Kutchen", 1988);
+        Person familyMember5 = new Person("Tim Kutchen", 1990);
+        Person familyMember6 = new Person("Andrew Kutchen", 1993);
+        Person familyMember7 = new Person("Peter Kutchen", 1993);
+        Person familyMember8 = new Person("Trudy Kutchen", 1996);
+
+
+
+    }
+}
